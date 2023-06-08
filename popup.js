ff710b0cf0ab6cd957f1c231855abbfdf5d449b3
kc-code32 <kelvinchen138@gmail.com>
2023-06-07 21:44:10 -0500

    last one

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentListener', () => {
-  let link = document.getElementById('popup-button');
-  link.addEventListener('click', () => {
-    alert('You clicked me!')
-  })
-})
